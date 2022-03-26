87407e90a8efc6151942a981d7a799a6467cace0
windyoung <windyoung89@gmail.com>
2022-03-27 00:22:55 +0800

    Done

--- SchulteGrid.py
+++ SchulteGrid.py
@@ -1,7 +1,12 @@
 from email import header
 import random
+import labels
+import os
+from reportlab.graphics import shapes
 # import prettytable
 from prettytable import PrettyTable, MSWORD_FRIENDLY, ALL
+from reportlab.pdfbase.pdfmetrics import registerFont
+from reportlab.pdfbase.ttfonts import TTFont
 
 
 class SchulteGrid():
@@ -9,7 +14,7 @@ class SchulteGrid():
         pass
 
     def GenNumbers(self, size_):
-        theGrid = ""
+        "生成数字随机序列"
         self.size_ = size_
         self.max_ = size_*size_
         numbers_ = list(range(1, self.max_+1))
@@ -19,19 +24,105 @@ class SchulteGrid():
         return numbers_
 
     def DrawTableSingle(self, thenumbers_):
+        "绘制方格"
         print('-' * 25)
         table_ = PrettyTable(border=True, header=False, hrules=ALL)
-        # table_.set_style(MSWORD_FRIENDLY)
         i = 0
         while i < self.max_:
             table_.add_row(thenumbers_[i: i+self.size_])
             i += self.size_
-        print(table_)
+        res_ = table_.get_string()
+        print(res_)
+        return res_
+
+    def GenGridPdf(self, data: list):
+        "生成PDF 文件"
+        # Create an A4 portrait (210mm x 297mm) sheets with 2 columns and 3 rows of labels. Each label is 80mm x 80mm with a 1mm rounded corner. The margins are automatically calculated.
+        # 创建A4 页面 ，一页最大布局8个舒尔特方格
+        specs = labels.Specification(210, 297, 2, 4, 72, 72, corner_radius=1)
+        # 设置字体 为楷体后能美化展示
+        base_path = os.path.dirname(__file__)
+        registerFont(TTFont('kaiti', os.path.join(base_path, 'simkai.ttf')))
+        # Create a function to draw each label. This will be given the ReportLab drawing  object to draw on, the dimensions (NB. these will be in points, the unit  ReportLab uses) of the label, and the object to render.
+        #
+
+        def draw_grids(label, width, height, obj):
+            "逐行添加舒尔特方格"
+            data_ = str(obj).split('\n')
+            lines_ = len(data_)
+            # 为不同大小的方格设置格式
+            if lines_-1 == 4:  # 2*2
+                for index, i_ in enumerate(data_, 1):
+                    s = shapes.String(2, height-32*index, i_,
+                                      fontSize=36, fontName="kaiti")
+                    label.add(s)
+            elif lines_-1 == 6:  # 3*3
+                for index, i_ in enumerate(data_, 1):
+                    s = shapes.String(2, height-24*index, i_,
+                                      fontSize=22, fontName="kaiti")
+                    label.add(s)
+            elif lines_-1 == 8:  # 4*4
+                for index, i_ in enumerate(data_, 1):
+                    s = shapes.String(2, height-20*index, i_,
+                                      fontSize=18, fontName="kaiti")
+                    label.add(s)
+            elif lines_-1 == 10:  # 5*5
+                for index, i_ in enumerate(data_, 1):
+                    s = shapes.String(2, height-5-16*index,
+                                      i_, fontSize=14, fontName="kaiti")
+                    label.add(s)
+            elif lines_-1 == 12:  # 6*6
+                for index, i_ in enumerate(data_, 1):
+                    s = shapes.String(2, height-10-12*index,
+                                      i_, fontSize=12, fontName="kaiti")
+                    label.add(s)
+
+        # Create the sheet.
+        sheet = labels.Sheet(specs, draw_grids, border=True)
+        # sheet.partial_page(1, ((1, 1), (2, 2), (4, 2)))
+        # sheet.partial_page(2, ((2, 1), (3, 1)))
+        # sheet.add_labels(range(3, 10))
+        for i in data:
+            sheet.add_label(i, 1)
+        # 输出文件时间戳
+        from datetime import datetime
+        t = datetime.now()
+        tString = t.strftime('%Y-%m-%d-%H-%M-%S')
+        # 输出PDF文件
+        sheet.save(f'SchuiteGrid_lable_{tString}.pdf')
 
 
 if __name__ == '__main__':
     thegrid_ = SchulteGrid()
-    thegrid_.DrawTableSingle(thegrid_.GenNumbers(4))
-    thegrid_.DrawTableSingle(thegrid_.GenNumbers(5))
+    # thegrid_.DrawTableSingle(thegrid_.GenNumbers(4))
+    a = []
+    # 一页最大8个舒尔特方格,规格设置2-6行宫格
 
-# JS 生成舒尔特方格？ 
+    # 第一页
+    def page_1():
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(3)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(3)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(4)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(4)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(3)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(3)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(4)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(4)))
+    # 第二页
+
+    def page_2():
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(2)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(3)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(4)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(4)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(5)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(5)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(6)))
+        a.append(thegrid_.DrawTableSingle(thegrid_.GenNumbers(6)))
+    # page_1()
+    page_2()
+
+    thegrid_.GenGridPdf(a)
+
+
+# JS 生成舒尔特方格？
--- test_pylable.py
+++ test_pylable.py
@@ -389,6 +389,73 @@ def test_schuiteGrid_lable():
 
     # Create the sheet.
     sheet = labels.Sheet(specs, draw_label, border=True)
+    sheet.partial_page(1, ((1, 1), (2, 2), (4, 2)))
+    sheet.add_label("Hello")
+    sheet.add_label("World")
+    sheet.partial_page(2, ((2, 1), (3, 1)))
+    sheet.add_labels(range(3, 10))
+    sheet.add_label("Oversized label here")
+    sheet.save('test_schuiteGrid_lable.pdf')
+    
+
     pass
 
-test_schuiteGrid_lable()
+def demo_nametag():
+    import labels
+    import os.path
+    from reportlab.pdfbase.ttfonts import TTFont
+    from reportlab.pdfbase.pdfmetrics import registerFont, stringWidth
+    from reportlab.graphics import shapes
+    from reportlab.lib import colors
+    import random
+    random.seed(187459)
+
+    # Create an A4 portrait (210mm x 297mm) sheets with 2 columns and 8 rows of
+    # labels. Each label is 90mm x 25mm with a 2mm rounded corner. The margins are
+    # automatically calculated.
+    specs = labels.Specification(210, 297, 2, 8, 90, 25, corner_radius=2)
+
+    # Get the path to the demos directory.
+    base_path = os.path.dirname(__file__)
+
+    # Add some fonts.
+    registerFont(TTFont('Judson Bold', os.path.join(base_path, 'Judson-Bold.ttf')))
+    registerFont(TTFont('KatamotzIkasi', os.path.join(base_path, 'KatamotzIkasi.ttf')))
+
+    # Create a function to draw each label. This will be given the ReportLab drawing
+    # object to draw on, the dimensions (NB. these will be in points, the unit
+    # ReportLab uses) of the label, and the name to put on the tag.
+    def write_name(label, width, height, name):
+        # Write the title.
+        label.add(shapes.String(5, height-20, "Hello, my name is",
+                                fontName="Judson Bold", fontSize=20))
+
+        # Measure the width of the name and shrink the font size until it fits.
+        font_size = 50
+        text_width = width - 10
+        name_width = stringWidth(name, "KatamotzIkasi", font_size)
+        while name_width > text_width:
+            font_size *= 0.8
+            name_width = stringWidth(name, "KatamotzIkasi", font_size)
+
+        # Write out the name in the centre of the label with a random colour.
+        s = shapes.String(width/2.0, 15, name, textAnchor="middle")
+        s.fontName = "KatamotzIkasi"
+        s.fontSize = font_size
+        s.fillColor = random.choice((colors.black, colors.blue, colors.red, colors.green))
+        label.add(s)
+
+    # Create the sheet.
+    sheet = labels.Sheet(specs, write_name, border=True)
+    sheet.partial_page(1, ((1, 1), (2, 2), (4, 2)))
+
+    # Use an external file as the data source. NB. we need to remove the newline
+    # character from each line.
+    with open(os.path.join(base_path, "names.txt")) as names:
+        sheet.add_labels(name.strip() for name in names)
+
+    # Save the file and we are done.
+    sheet.save('nametags.pdf')
+    print("{0:d} label(s) output on {1:d} page(s).".format(sheet.label_count, sheet.page_count))
+
+demo_nametag()
